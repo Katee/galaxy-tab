8c4119f936df436f2b0d281f7e5ac7a3eafa678e
Josh Teneycke <josh.teneycke@gmail.com>
2012-10-24 02:24:23 -0400

    merge colors with longer timeout

--- js/historygraph.js
+++ js/historygraph.js
@@ -15,15 +15,9 @@ function init() {
       defaultEdgeType: 'curve'
     }).graphProperties({
       minNodeSize: 0.5,
-<<<<<<< HEAD
       maxNodeSize: 5,
       minEdgeSize: 3,
       maxEdgeSize: 4
-=======
-      maxNodeSize: 3,
-      minEdgeSize: 1,
-      maxEdgeSize: 2
->>>>>>> e5f7b8d46d95670c64adbd9faace2236304686d1
     }).mouseProperties({
         maxRatio: 4
     });
@@ -61,13 +55,8 @@ function init() {
       toggleAtlas();
       
       // stop altas afer 4 seconds, makes it more friendly for background tabs
-<<<<<<< HEAD
-      setTimeout(function(){toggleAtlas();}, 14000);
-    }, 100);
-=======
       setTimeout(function(){toggleAtlas();}, 4000);
     }, 1000);
->>>>>>> e5f7b8d46d95670c64adbd9faace2236304686d1
     // get rid of this timeout
   });
 }
