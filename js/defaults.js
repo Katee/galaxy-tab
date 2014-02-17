776c8e9c1fcda9319c787f9ba92fb83f6f022d09
PLR <plredmond@gmail.com>
2014-02-17 16:06:23 -0500

    made options easier to extend

---
+++ js/defaults.js
@@ -0,0 +1,31 @@
+// Configuration for each option (default values, etc..)
+// uses: js/widgets.js
+
+options_config = { conf_charge:        {widget:RangeWidget, value:-70  , label:'Charge',                min: -200, max:60, step:5   }
+                 , conf_distance:      {widget:RangeWidget, value: 25  , label:'Distance' ,             min:    0, max:50, step:1   }
+                 , conf_linkmindark:   {widget:RangeWidget, value:  0.5, label:'Link Minimum Darkness', min:    0, max: 1, step:0.05}
+                 , conf_noderadius:    {widget:RangeWidget, value:  5  , label:'Node Radius',           min:    0, max:10, step:0.5 }
+                 , opt_showsingletons: {widget:BoolWidget , value: true, label:'Show unlinked Nodes?'                               }
+                 };
+
+// Read all options from local storage (or provide a default value).
+function read_options() {
+    var saved = localStorage['options'],
+        options = saved == undefined ? {} : JSON.parse(saved);
+    // add in missing things
+    for(var name in options_config){
+        if(options_config.hasOwnProperty(name)){
+            if(!options.hasOwnProperty(name)){
+                console.log('Adding default value for new option:', name);
+                options[name] = options_config[name].value;
+            }else
+            if(typeof options[name] != typeof options_config[name].value){
+                console.log('Swapping-in default value for wrong type on:', name);
+                options[name] = options_config[name].value;
+            }
+        }
+    }
+    return options;
+}
+
+// eof
--- js/options.js
+++ js/options.js
@@ -1,43 +1,80 @@
-// write all options to local storage
-function save_options(evt) {
-    localStorage['options'] = JSON.stringify({
-        conf_charge:        $('#conf_charge'       ).val(),
-        conf_distance:      $('#conf_distance'     ).val(),
-        conf_linkmindark:   $('#conf_linkmindark'  ).val(),
-        conf_noderadius:    $('#conf_noderadius'   ).val(),
-        opt_showsingletons: $('#opt_showsingletons').prop('checked')
-        });
+// (main) An options page for galaxy tab.
+// uses: js/defaults.js
 
+state = {};
+
+// Update options page to display the configuration object
+function restore_options() {
+    var table = $('table#options'),
+        options = read_options(),
+        widgets = {};
+    // build widgets and set their values
+    for(var name in options_config){
+        if(options_config.hasOwnProperty(name)){
+            widgets[name] = add_widget_to_table(table, name, options_config[name]);
+            widgets[name].write(options[name]);
+        }
+    }
+    // save the widgets for later (in save_options)
+    state.widgets = widgets;
+}
+
+// Add the widget specified to the table and return it.
+// JQuery Element Set, String, Object -> Widget
+function add_widget_to_table(table, name, conf){
+    var label = $('<label>'),
+        td1 = $('<td>'),
+        td2 = $('<td>'),
+        widget = make_widget(name, conf),
+        tr = $('<tr>');
+    // label
+    label.prop('for', name);
+    label.text(conf.label);
+    td1.append(label);
+    tr.append(td1);
+    // widget
+    td2.append(widget.jel);
+    tr.append(td2);
+    // table
+    table.prepend(tr);
+    return widget;
+}
+
+// Literally just return the widget specified by the conf.
+// String, Object -> Widget
+function make_widget(name, conf){
+    var w = undefined;
+    switch(conf.widget){
+        case RangeWidget:
+            w = new conf.widget(name, conf.value, conf.min, conf.max, conf.step);
+            monitor(w);
+            break;
+        case BoolWidget:
+            w = new conf.widget(name, conf.value);
+            break;
+        default:
+            throw "append_widget: Not implemented (yet!) for the given widget type.";
+            break;
+    }
+    return w;
+}
+
+// Write all options in html to local storage.
+function save_options(evt) {
+    var options = {};
+    // read the widgets
+    for(var name in state.widgets){
+        if(state.widgets.hasOwnProperty(name)){
+            options[name] = state.widgets[name].read();
+        }
+    }
+    // save
+    localStorage['options'] = JSON.stringify(options);
     // provide feedback
     $('#status').text('Options saved.');
     setTimeout(function() {
             $('#status').text('');
             }, 750);
-}
-
-// add a span to monitor a ranged input
-function monitor(jel){
-    var span = $('<span>');
-    span.text(jel.val());
-    jel.after(span);
-    jel.on('change', function() {
-            span.text(jel.val());
-            });
-}
-
-// update options page to display a configuration object
-function restore_options() {
-    options = read_options();
-    $('#conf_charge'       ).val(options.conf_charge     );
-    $('#conf_distance'     ).val(options.conf_distance   );
-    $('#conf_linkmindark'  ).val(options.conf_linkmindark);
-    $('#conf_noderadius'   ).val(options.conf_noderadius );
-    $('#opt_showsingletons').prop('checked', options.opt_showsingletons);
-
-    monitor($('#conf_charge'     ));
-    monitor($('#conf_distance'   ));
-    monitor($('#conf_linkmindark'));
-    monitor($('#conf_noderadius' ));
 }
 
 $(document).ready(restore_options);
